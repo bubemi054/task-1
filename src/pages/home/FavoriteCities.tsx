b8f5c8edc67389d131c6d6bbd00bc38c0d8abb43
deji100 <adeyemideji9@gmail.com>
2025-03-18 20:39:15 +0100

    updated ui

--- src/pages/home/FavoriteCities.tsx
+++ src/pages/home/FavoriteCities.tsx
@@ -30,15 +30,13 @@ export default function FavoriteCities() {
     return sortedCities;
   }, [citiesWeather, favoriteCities, removedCities]);
 
-  const showRemovedCitiesBtn = removedCities?.length > 0;
-
   return (
     <>
       <div className="mb-[1rem]">
         <div className="mb-[2.7rem] flex justify-between items-flex-end">
           <Header1 className="">Favorite Cities</Header1>
           <div className="flex flex-col items-end gap-2 relative">
-            {showRemovedCitiesBtn && (
+            {removedCities?.length > 0 && (
               <button
                 onClick={() => dispatch(uiActions.toggleShowRemovedCities())}
                 className="inline-flex cursor-pointer justify-center items-center pl-[1.5625rem] pr-6 py-[0.8125rem] rounded-[0.9375rem] bg-white text-black text-base not-italic font-bold leading-[normal] font-manrope"
