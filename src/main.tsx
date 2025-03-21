cb841ea0d66c8a27301d293b167b18878ee42eb5
deji100 <adeyemideji9@gmail.com>
2025-03-21 13:44:53 +0100

    updated main

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
-import { store } from "./state-manager/store.ts";
+import { store, persistor } from "./state-manager/store.ts";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
